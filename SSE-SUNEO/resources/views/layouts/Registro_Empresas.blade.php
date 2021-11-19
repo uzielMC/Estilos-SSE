d1c5e461a58961ef6f9205c2cccc423339e74aff
Luis Enrique Reyes Martínez <luis.enrique.68828@gmail.com>
2021-11-18 20:46:53 -0600

    Actualización de las rutas para las vista. También se actualizo la vista layout Registro_Empresas

--- SSE-SUNEO/resources/views/layouts/Registro_Empresas.blade.php
+++ SSE-SUNEO/resources/views/layouts/Registro_Empresas.blade.php
@@ -5,8 +5,8 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ingreso Perfil de la Empresa</title>
-        <link rel="stylesheet" href="{{ asset('css/Ingreso_perfil_de_la_empresa.css') }}">
+        <title>@yield('title')</title>
+        @yield('styles')        
     </head>
     
     <body>
--- SSE-SUNEO/routes/web.php
+++ SSE-SUNEO/routes/web.php
@@ -7,6 +7,8 @@ Route::get('/', function () {
 })->name('welcome');
 
 Route::prefix('Empresas')->group(function () {
-    Route::get('/', 'RegistroEmpresaController@view');
+    Route::get('/perfil-empresa', 'RegistroEmpresaController@view_perfil_empresa')->name('perfil-empresa');
+    Route::get('/encuesta-de-satisfaccion', 'RegistroEmpresaController@view_encuesta_satisfaccion');
     Route::post('/create', 'RegistroEmpresaController@create')->name('Empresa.create');
+    Route::post('/stored_encuesta', 'RegistroEmpresaController@stored_encuesta')->name('Empresa.stored_encuesta');
 });
